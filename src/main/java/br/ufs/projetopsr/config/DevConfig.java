ad9d1ae7bf4a08ca9ce097d831246ed285bda83b
juanbomfim22 <juanbomfim22@gmail.com>
2022-05-03 10:21:41 -0300

    Configurando ambiente producao

--- src/main/java/br/ufs/projetopsr/config/DevConfig.java
+++ src/main/java/br/ufs/projetopsr/config/DevConfig.java
@@ -1,26 +1,33 @@
 package br.ufs.projetopsr.config;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import br.ufs.projetopsr.services.DBService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
 
-//	@Autowired
-//	private DBService dbService;
+	@Autowired
+	private DBService dbService;
 
 	// Pega a informação do application-dev.properties e coloca na variável strategy
-//	@Value("${spring.jpa.hibernate.ddl-auto}")
-//	private String strategy;
-//
-//	@Bean
-//	public boolean instantiateDatabase() throws ParseException {
-//		if (!"create".equals(strategy)) {
-//			return false;
-//		}
-//		dbService.instantiateTestDatabase();
-//		return true;
-//
-//	}
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+
+	}
 }
