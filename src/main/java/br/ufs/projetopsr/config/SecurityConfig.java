04463a9cc063b463b50aa67dd28ffe5373529555
juanbomfim22 <juanbomfim22@gmail.com>
2022-05-03 10:33:07 -0300

    Adicionando caminhos abertos

--- src/main/java/br/ufs/projetopsr/config/SecurityConfig.java
+++ src/main/java/br/ufs/projetopsr/config/SecurityConfig.java
@@ -46,12 +46,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
-			"/produtos/**",
- 			"/categorias/**"
+			"/docentes/**",
+ 			"/grades/**",
+ 			"/disciplinas/**",
+ 			"/cursos/**",
+ 			"/usuarios/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**", 
+			"/usuarios/**", 
 			"/auth/forgot/**",
 	};
 	
