4f92670f5e71fb8e3de5e3440b3112cd2e5709a4
juanbomfim22 <juanbomfim22@gmail.com>
2022-05-03 21:38:14 -0300

    Retirando error-controller de Swagger-UI

--- src/main/java/br/ufs/projetopsr/config/SpringFoxConfig.java
+++ src/main/java/br/ufs/projetopsr/config/SpringFoxConfig.java
@@ -1,10 +1,17 @@
 package br.ufs.projetopsr.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,10 +19,29 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SpringFoxConfig {
 	@Bean
 	public Docket swagger() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
-			.build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.ufs.projetopsr.resources"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo())
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, List.of(
+					new ResponseBuilder().code("200")
+					        .description("200 message").build(),
+					new ResponseBuilder().code("403")
+					        .description("403 message").build(),
+				    new ResponseBuilder().code("500")
+				        .description("500 message").build()
+				));
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo("API - PSR",
+				"Esta é a API para o projeto PSR", "Versão 1.0",
+				"",
+				new Contact(null,null,null),
+				"", "", Collections.emptyList() // Vendor
+																										// Extensions
+		);
 	}
 }
